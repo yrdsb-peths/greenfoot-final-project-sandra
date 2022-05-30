3a088173a5be07c813c7a23f770b2eb4fb3e8e0e
sandra2322 <101139244+sandra2322@users.noreply.github.com>
2022-05-30 14:21:03 -0400

    messing around with fonts

--- Button.java
+++ Button.java
@@ -2,12 +2,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Button extends Actor
 {
+    private static final Color bgColour = new Color(255, 76, 76);
+    private static final Color txtColour = Color.WHITE;
+    private static final Font font = new Font("Times New Roman", 40);
+    
+    private GreenfootImage text;
+    private int textSize = 40;
+    
     private int cost;
     private String name;
     private boolean canBuy;
     
     public Button(String name, int cost){
-        getImage().scale(70, 50);
+        setImage(new GreenfootImage("", 50, txtColour, bgColour));
+        text = getImage();
+        text.scale(70, 50);
         this.name = name;
         this.cost = cost;
     }
@@ -27,9 +36,13 @@ public class Button extends Actor
     }
     public void showText(){
         if(cost!=0){
-            getWorld().showText(name + "\n$" + cost, getX(), getY());
+            //getWorld().showText(name + "\n$" + cost, getX(), getY());
+            text = new GreenfootImage(name + "\n$" + cost, 30, txtColour, bgColour);
+            setImage(text);
         }else{
-            getWorld().showText(name, getX(), getY());
+            //getWorld().showText(name, getX(), getY());
+            text = new GreenfootImage(name, 30, txtColour, bgColour);
+            setImage(text);
         }
     }
     public void buy(){
